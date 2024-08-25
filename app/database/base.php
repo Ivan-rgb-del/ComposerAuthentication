755d216fda3902006b317a5b9c711004695dc252
Ivan Knezevic <knezevicivan500@gmail.com>
2024-08-25 22:00:54 +0200

    Connect to database

---
+++ app/database/base.php
@@ -0,0 +1,29 @@
+<?php
+
+  class Base {
+    const HOST = "localhost";
+    const DB_USERNAME = "root";
+    const DB_PASSWORD = "";
+    const DB_NAME = "authentication";
+    const PORT = 4306;
+
+    protected $conn;
+
+    public function __construct() {
+      $this->conn = mysqli_connect(self::HOST, self::DB_USERNAME, self::DB_PASSWORD, self::DB_NAME, self::PORT);
+
+      if ($this->conn->connect_error) {
+        die("Connection failed: " . $this->conn->connect_error);
+      }
+    }
+
+    public function query($sql) {
+      return $this->conn->query($sql);
+    }
+
+    public function prepare($sql) {
+      return $this->conn->prepare($sql);
+    }
+  }
+
+?>
